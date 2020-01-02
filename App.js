1cedd22792d697d47c737044ee6773820d03e52c
Douglas Fanucchi <douglasfanucchi@gmail.com>
2020-01-02 19:23:02 -0300

    instala dependencias

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack'
 
 export default function App() {
+  console.log(createStackNavigator)
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
