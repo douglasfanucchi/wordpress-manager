8a6b99da01ba0f90e25667dc08e73e6d6d31a277
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-01-03 13:14:59 -0300

    Organizando arquivos do novo componente do custom drawer

--- App.js
+++ App.js
@@ -3,16 +3,10 @@ import { createAppContainer } from "react-navigation";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { StyleSheet, ScrollView, View, Text, Image } from "react-native";
 
 import Reducers from "./src/reducers";
-
 import Posts from "./src/screens/Posts";
-
-import SafeAreaView from "react-native-safe-area-view";
-import { DrawerItems } from "react-navigation-drawer";
-import { colors } from "./src/constants/colors";
-import { LinearGradient } from "expo-linear-gradient";
+import CustomDrawerContentComponent from './src/components/custom-drawer';
 
 const store = createStore(Reducers);
 
@@ -39,73 +33,3 @@ export default function App() {
   );
 }
 
-const CustomDrawerContentComponent = props => (
-  <ScrollView>
-    <SafeAreaView
-      style={styles.container}>
-      <LinearGradient
-        style={styles.header}
-        colors={[colors.primary, colors.secondary]}>
-          <View style={styles.userinfo}>
-            <Text style={styles.username}>Felipe Fanucchi</Text>
-            <Text style={styles.useremail}>flfanucchi@gmail.com</Text>
-          </View>
-        <View style={styles.avatar}>
-          <Image source={require('./assets/avatar-not-logged.png')} style={styles.avatarImage}></Image>
-        </View>
-
-      </LinearGradient>
-
-      <View style={styles.drawerItems}>
-        <DrawerItems {...props}/>
-      </View>
-    </SafeAreaView>
-  </ScrollView>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  header: {
-    height: 100,
-    backgroundColor: colors.primary
-  },
-  avatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    backgroundColor: "#FFF",
-    position: "absolute",
-    bottom: -25,
-    left: 105
-  },
-  drawerItems: {
-    marginTop: 30
-  },
-  avatarImage: {
-    resizeMode: 'cover',
-    width: 50,
-    height: 50,
-    borderRadius: 50,
-    borderWidth: 2,
-    borderColor: "#fff",
-  },
-  userinfo: {
-    justifyContent: 'center',
-    alignItems: "center",
-    flex: 1,
-    marginBottom: 20
-  },
-  username: {
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: '600',
-    marginBottom: 5,
-  },
-  useremail: {
-    color: "#FFF",
-    fontSize: 12,
-    fontWeight: '100',
-  }
-});
---
+++ src/components/custom-drawer.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { StyleSheet, ScrollView, View, Text, Image } from "react-native";
+import SafeAreaView from "react-native-safe-area-view";
+import { DrawerItems } from "react-navigation-drawer";
+import { colors } from "../constants/colors";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default CustomDrawerContentComponent = props => (
+    <ScrollView>
+      <SafeAreaView
+        style={styles.container}>
+        <LinearGradient
+          style={styles.header}
+          colors={[colors.primary, colors.secondary]}>
+            <View style={styles.userinfo}>
+              <Text style={styles.username}>Felipe Fanucchi</Text>
+              <Text style={styles.useremail}>flfanucchi@gmail.com</Text>
+            </View>
+          <View style={styles.avatar}>
+            <Image source={require('../../assets/avatar-not-logged.png')} style={styles.avatarImage}></Image>
+          </View>
+  
+        </LinearGradient>
+  
+        <View style={styles.drawerItems}>
+          <DrawerItems {...props}/>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    header: {
+      height: 100,
+      backgroundColor: colors.primary
+    },
+    avatar: {
+      width: 50,
+      height: 50,
+      borderRadius: 50,
+      backgroundColor: "#FFF",
+      position: "absolute",
+      bottom: -25,
+      left: 110
+    },
+    drawerItems: {
+      marginTop: 30
+    },
+    avatarImage: {
+      resizeMode: 'cover',
+      width: 50,
+      height: 50,
+      borderRadius: 50,
+      borderWidth: 2,
+      borderColor: "#fff",
+    },
+    userinfo: {
+      justifyContent: 'center',
+      alignItems: "center",
+      flex: 1,
+      marginBottom: 20
+    },
+    username: {
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: '600',
+      marginBottom: 5,
+    },
+    useremail: {
+      color: "#FFF",
+      fontSize: 12,
+      fontWeight: '100',
+    }
+  });
+  
